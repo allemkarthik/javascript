11e5f0ed3835b886b07732364ec4080c3b7430eb
allemkarthik <allemkarthik@gmail.com>
2024-09-19 20:34:34 +0530

    day3 updated

---
+++ day3js/day3-1.js
@@ -0,0 +1,49 @@
+// task1
+// arithematic opertaor
+var a=10;
+var b=20;
+
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(a%b);
+console.log(a++);
+console.log(a--);
+console.log(++a);
+
+// assignment operator
+
+var c=100;
+var d=c+10;
+var e=c-10;
+var f=c*10;
+var g=c/10;
+var h=c%10;
+var i=c--;
+var j=c++;
+
+console.log(d,e,f,g,h,i,j)
+
+
+// comparsion operator
+
+var kar=20
+var sai=30
+console.log(kar==sai)
+console.log(kar===sai)  //strict equal operator
+console.log(kar!=sai)
+console.log(kar!==sai)  //strcit not equal operator 
+console.log(kar>sai)
+console.log(kar>=sai)
+console.log(kar<sai)
+console.log(kar<=sai)
+
+
+//logical opertor
+
+console.log(kar==sai && kar>=sai)    //and operation both conditons should satisfy
+console.log(kar==sai || kar<=sai)    //or operation only one conditons should satisfy
+
---
+++ day3js/day3-2.js
@@ -0,0 +1,14 @@
+// task2 even or odd using ternery opertor
+
+var a=10;
+var b=a%2==0 ? "it is even": "it is odd";
+console.log(b)
+
+
+
+
+// task3 check whether user is an adult or minor
+
+var user=16;
+var check= user>=18 && user<=60 ? "given user is an adult" : "given user is minor"
+console.log(check)
