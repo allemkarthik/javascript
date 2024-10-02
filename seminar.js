a62bfd9515817cfabef516971fb36afca7c08227
allemkarthik <allemkarthik@gmail.com>
2024-10-02 17:24:34 +0530

    dom

--- seminar.js
+++ seminar.js
@@ -1,26 +1,47 @@
-// check whether given stirg is vowel or not
+// // check whether given stirg is vowel or not
 
-function find(word){
+// function find(word){
 
-    vowels=['a','e','i','o','u']
-    givenstr=false;
-    for (i=0;i<=word.length;i++){
-        if (vowels.includes(word[i])){
-            givenstr=true
-            break;
+//     vowels=['a','e','i','o','u']
+//     givenstr=false;
+//     for (i=0;i<=word.length;i++){
+//         if (vowels.includes(word[i])){
+//             givenstr=true
+//             break;
             
-        }
-    }
-    if(givenstr==true){
-        console.log("vowel")
-    }
-    else{
-        console.log("consonent")
-    }
+//         }
+//     }
+//     if(givenstr==true){
+//         console.log("vowel")
+//     }
+//     else{
+//         console.log("consonent")
+//     }
+// }
+// find("k")
 
 
+// karthik is study 
+// karthik got skils
+// karthik got job
+// karthik got married
 
+function study(a){
+    console.log("karthik is studying");
+    a(job)
+    
 }
 
+function married(){
+    console.log("kar got married")
+}
+function skill(b){
+    console.log("kar got skills")
+    b(married)
+}
+function job(c){
+    console.log("kar got job")
+    c()
+}
 
-find("k")
+study(skill)
